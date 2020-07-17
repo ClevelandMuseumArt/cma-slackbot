5c835f71a06d84001b982ff9d0d24a68a25edc06
Ethan Holda <ethan@plutino.net>
2020-07-17 16:45:41 -0400

    Refactor auth changes.

--- app.js
+++ app.js
@@ -30,17 +30,6 @@ const promptsUrl = process.env["PROMPT_URL"];
 // authenticate
 axios.defaults.headers.common['Authentication'] = process.env['SLACK_BOT_API_TOKEN'];
 
-const getAuth = async(teamId) => {
-    const auth_url = `${slackBotApiUrl}auth_post/${teamId}`;
-    const auth_result = await axios.get(auth_url);
-
-    return {
-        botToken: results.botToken,
-        botId: results.botId,
-        botUserId: results.botUserId
-    };
-}
-
 const getTokenData = async (teamId) => {
   const tokenUrl = `${slackBotApiUrl}tokens/${teamId}`;
   
@@ -259,15 +248,13 @@ const stateClearUserData = async (teamId) => {
 // END STATE API FUNCTIONS
 
 const writeExhibitionToAPI = async (slackbotId, data) => {
-  getAuth();
-	
   var req = {
     slackbot_id: slackbotId,
     data: data
   };
 
   try {
-    var resp = await axios.post(slackBotApiUrl, req);
+    var resp = await axios.post(`${slackBotApiUrl}slackbot-data`, req);
 
     console.log("writeExhibitionToAPI");
   } catch (error) {
